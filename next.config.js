2e44318dcb02b4e0bf92d21b27bf69cd1a7ff719
familyman8012 <familyman801205@gmail.com>
2021-08-05 00:22:26 +0900

    builder오류

--- next.config.js
+++ next.config.js
@@ -5,4 +5,23 @@ module.exports = {
     loader: "imgix",
     path: "",
   },
+  plugins: [
+    //ignore the drivers you don't want. This is the complete list of all drivers -- remove the suppressions for drivers you want to use.
+    new FilterWarningsPlugin({
+      exclude: [
+        /mssql/,
+        /mysql/,
+        /mysql2/,
+        /oracledb/,
+        /pg/,
+        /pg-native/,
+        /pg-query-stream/,
+        /react-native-sqlite-storage/,
+        /redis/,
+        /sqlite3/,
+        /sql.js/,
+        /typeorm-aurora-data-api-driver/,
+      ],
+    }),
+  ],
 };
