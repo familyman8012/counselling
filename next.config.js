a588a6343a4129246924ef601541f1a9780bd26a
familyman8012 <familyman801205@gmail.com>
2021-08-05 01:34:48 +0900

    @prisma/client

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 module.exports = {
   reactStrictMode: true,
   target: "experimental-serverless-trace",
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.externals.push("@prisma/client");
+    }
+
+    return config;
+  },
   images: {
     loader: "imgix",
     path: "",
