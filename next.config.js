43b0e05612d46cf87b0591b7cf74157430a06d01
familyman8012 <familyman801205@gmail.com>
2021-08-05 01:44:42 +0900

    prisma@client 설치

--- next.config.js
+++ next.config.js
@@ -1,13 +1,6 @@
 module.exports = {
   reactStrictMode: true,
   target: "experimental-serverless-trace",
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      config.externals.push("@prisma/client");
-    }
-
-    return config;
-  },
   images: {
     loader: "imgix",
     path: "",
