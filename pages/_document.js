58f964a0788967c89946668b653d862fc07ffb38
familyman8012 <familyman801205@gmail.com>
2021-08-05 14:20:59 +0900

    이미지최적화, 스크립트

--- pages/_document.js
+++ pages/_document.js
@@ -1,13 +1,15 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
-import Script from "next/script";
 
 class MyDocument extends Document {
   render() {
     return (
       <Html lang="ko">
         <Head>
-          <Script src="https://cdn.bootpay.co.kr/js/bootpay-3.3.1.min.js"></Script>
-          <Script src="js/channeltalk.js"></Script>
+          <script
+            async
+            src="https://cdn.bootpay.co.kr/js/bootpay-3.3.1.min.js"
+          ></script>
+          <script async src="js/channeltalk.js"></script>
         </Head>
         <body>
           <Main />
